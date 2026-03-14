c20952bb37fc98e6250370e3cf8b46a326a6af3e
[Sayona] <[sayonanc10@gmail.com]>
2026-03-14 17:32:39 +0530

    Add deployment config: Procfile, runtime.txt, whitenoise, gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn buspass.wsgi --log-file -
--- buspass/settings.py
+++ buspass/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,6 +99,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
